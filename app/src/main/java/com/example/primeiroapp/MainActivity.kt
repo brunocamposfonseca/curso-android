d9dbed79c2b3f9acc58fc7581a4a99b814f0d860
brunocamposfonseca <brunocamposfonsecaofc@gmail.com>
2024-09-15 12:17:02 -0300

    first commit

--- app/src/main/java/com/example/primeiroapp/MainActivity.kt
+++ app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -1,20 +1,49 @@
+//AULA - 001
+
+//package com.example.primeiroapp
+
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//import com.example.primeiroapp.databinding.ActivityMainBinding
+
+//class MainActivity : AppCompatActivity() {
+
+    //private lateinit var binding: ActivityMainBinding
+
+    //override fun onCreate(savedInstanceState: Bundle?) {
+        //super.onCreate(savedInstanceState)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(binding.root)
+
+        //binding.buttonHello.setOnClickListener {
+            //val name = binding.inputName.text.toString()
+
+            //binding.textResult.text = "Olá, " + name
+            //binding.textResult.text = "Olá, ${name}"
+            //binding.textResult.setText("Olá, " + name)
+
+        //}
+
+    //}
+//}
+
+//*-------------------------------------------------------------------------------------------------------*
+
+//AULA - 002
+
 package com.example.primeiroapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.primeiroapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
     }
 }
